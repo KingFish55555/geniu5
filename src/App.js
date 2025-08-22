5ebd437caaaded23dabd71e8097275e2e56fd0d7
KingFish55555 <wangfluter@gmail.com>
2025-08-23 04:09:50 +0800

    0.4.11 修改向XX說話的文字高度

--- src/App.js
+++ src/App.js
@@ -1771,7 +1771,7 @@ const SettingsPage = ({
               <div className="card-content">
                 <div className="about-info">
                   <h4>GENIU5</h4>
-                  <p>版本：0.4.1</p>
+                  <p>版本：0.4.11</p>
                   <p>為了想要在手機上玩AI的小東西</p>
                 </div>
                 <div className="about-links">
