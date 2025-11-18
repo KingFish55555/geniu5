a9c592cf41d9afe7866ffbba1d87a13878a43397
KingFish55555 <wangfluter@gmail.com>
2025-11-19 00:29:25 +0800

    ais消失

--- src/App.js
+++ src/App.js
@@ -2543,7 +2543,7 @@ const ChatApp = () => {
     gemini: {
       name: 'Google Gemini',
       endpoint: 'https://generativelanguage.googleapis.com/v1beta/models/',
-      models: ['gemini-3-pro-preview','gemini-3-pro-preview-ais-applets','gemini-2.5-flash', 'gemini-2.5-pro', 'gemini-2.5-pro-preview-06-05', 'gemini-2.5-pro-preview-05-06', 'gemini-2.5-pro-preview-03-25', 'gemini-2.5-pro-exp-03-25', 'gemini-2.5-flash-preview-05-20', 'gemini-2.5-flash-preview-04-17',],
+      models: ['gemini-3-pro-preview','gemini-2.5-flash', 'gemini-2.5-pro', 'gemini-2.5-pro-preview-06-05', 'gemini-2.5-pro-preview-05-06', 'gemini-2.5-pro-preview-03-25', 'gemini-2.5-pro-exp-03-25', 'gemini-2.5-flash-preview-05-20', 'gemini-2.5-flash-preview-04-17',],
       headers: () => ({ 'Content-Type': 'application/json' }),
       isGemini: true
     },
