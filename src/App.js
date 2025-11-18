fbae33aa45fb76d424bcf37983cfa13da8bc9a40
KingFish55555 <wangfluter@gmail.com>
2025-11-18 23:19:22 +0800

    gemini 3

--- src/App.js
+++ src/App.js
@@ -2267,7 +2267,7 @@ const SettingsPage = ({
                 <div className="about-info">
                   <h4>GENIU5</h4>
                   <p>aka 55小手機</p>
-                  <p>版本：0.5.68</p>
+                  <p>版本：0.5.69</p>
                   <p>為了想要在手機上玩AI聊天的小東西</p>
                 </div>
                 <div className="about-links">
@@ -2543,7 +2543,7 @@ const ChatApp = () => {
     gemini: {
       name: 'Google Gemini',
       endpoint: 'https://generativelanguage.googleapis.com/v1beta/models/',
-      models: ['gemini-2.5-flash', 'gemini-2.5-pro', 'gemini-2.5-pro-preview-06-05', 'gemini-2.5-pro-preview-05-06', 'gemini-2.5-pro-preview-03-25', 'gemini-2.5-pro-exp-03-25', 'gemini-2.5-flash-preview-05-20', 'gemini-2.5-flash-preview-04-17',],
+      models: ['gemini-3-pro-preview','gemini-3-pro-preview-ais-applets','gemini-2.5-flash', 'gemini-2.5-pro', 'gemini-2.5-pro-preview-06-05', 'gemini-2.5-pro-preview-05-06', 'gemini-2.5-pro-preview-03-25', 'gemini-2.5-pro-exp-03-25', 'gemini-2.5-flash-preview-05-20', 'gemini-2.5-flash-preview-04-17',],
       headers: () => ({ 'Content-Type': 'application/json' }),
       isGemini: true
     },
